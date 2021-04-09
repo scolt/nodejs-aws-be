414faedb7e726b29d50e8c0b363da05e872f1078
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2021-04-09 20:25:36 +0300

    Rollback product structure

--- products-service/data/products.js
+++ products-service/data/products.js
@@ -1,71 +1,50 @@
 export default [
   {
     id: '1',
-    name: 'La Belle Russet',
+    title: 'La Belle Russet',
     image: 'https://www.potatogrower.com/Images/gallery/8880_600.jpg',
-    description: 'Thick skin with light and fluffy center',
-    attributes: {
-      type: 'Russet',
-      price: 56
-    },
+    description: 'Russet. Thick skin with light and fluffy center',
+    price: 56,
   },
   {
     id: '2',
-    name: 'Red Scarlet',
+    title: 'Red Scarlet',
     image: 'https://agrobelarus.com/uploads/products/belarus/kartofel/red_scarlet/4.jpg',
-    description: 'Thin skin and stays firm throughout cooking',
-    attributes: {
-      type: 'Red',
-      price: 71
-    },
+    description: 'Red. Thin skin and stays firm throughout cooking',
+    price: 71,
   },
   {
     id: '3',
-    name: 'Yukon Gold',
+    title: 'Yukon Gold',
     image:  'http://www.valleyspuds.com/wp-content/uploads/Yukon-Gold-Potatoes-500x333.jpg',
-    description: 'Buttery flavor with a creamy texture',
-    attributes: {
-      type: 'Yellow',
-      price: 108
-    },
+    description: 'Yellow. Buttery flavor with a creamy texture',
+    price: 108,
   },
   {
     id: '4',
-    name: 'Riverford',
+    title: 'Riverford',
     image: 'https://www.georgeperry.co.uk/images/P/whitepotato.jpg',
-    description: 'Thin skin with a nutty flavor and stays firm throughout cooking',
-    attributes: {
-      type: 'White',
-      price: 113
-    },
+    description: 'White. Thin skin with a nutty flavor and stays firm throughout cooking',
+    price: 113,
   },
   {
     id: '5',
-    name: 'Majesty',
+    title: 'Majesty',
     image: 'https://www.specialtyproduce.com/sppics/641.png',
-    description: 'Medium skin with an earthy flavor and vibrant color',
-    attributes: {
-      type: 'Purple',
-      price: 64
-    },
+    description: 'Purple. Medium skin with an earthy flavor and vibrant color',
+    price: 64,
   },
   {
     id: '6',
-    name: 'Russian Banana',
+    title: 'Russian Banana',
     image: 'https://www.specialtyproduce.com/sppics/133.png',
-    description: 'Nutty and buttery flavor with a firm texture',
-    attributes: {
-      type: 'Fingerling',
-      price: 47,
-    },
+    description: 'Fingerling. Nutty and buttery flavor with a firm texture',
+    price: 47,
   },
   {
     id: '7',
-    name: 'Baby Dutch',
+    title: 'Baby Dutch',
     image: 'https://cdn.shopify.com/s/files/1/0336/7167/5948/products/130-2_400x400.jpg?v=1583843577',
-    description: '',
-    attributes: {
-      type: 'Petite',
-      price: 55
-    },
+    description: 'Petite',
+    price: 55,
   }];
